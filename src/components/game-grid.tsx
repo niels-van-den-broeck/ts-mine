672a6493f5d1ec16d1185f7787206c9f0cc44b17
niels-van-den-broeck <niels.van.den.broeck@pelckmansuitgevers.be>
2019-12-15 20:14:51 +0100

    allow user to click field

--- src/components/game-grid.tsx
+++ src/components/game-grid.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer } from 'react';
+import React, { useEffect, useReducer, useCallback } from 'react';
 
 interface GridProps {
   difficulty: number;
@@ -10,6 +10,7 @@ interface Action {
 }
 
 interface Field {
+  id: string;
   isBomb: boolean;
   isActive: boolean;
   surroundingBombs?: number;
@@ -26,6 +27,7 @@ function reducer(state: Array<Array<Field>>, action: Action) {
 
         for (let x = 0; x < 25; x++) {
           grid[y][x] = {
+            id: `${x}-${y}`,
             isBomb: Math.random() < difficulty,
             isActive: false,
           };
@@ -34,7 +36,16 @@ function reducer(state: Array<Array<Field>>, action: Action) {
 
       return grid;
     }
-
+    case 'FIELD_OPENED': {
+      const { x, y } = action.payload;
+      return state.map((row, currY) =>
+        currY === y
+          ? row.map((field, currX) =>
+              currX === x ? { ...field, isActive: true } : field
+            )
+          : row
+      );
+    }
     default:
       return state;
   }
@@ -52,12 +63,24 @@ const GameGrid: React.FC<GridProps> = ({ difficulty }) => {
     });
   }, [difficulty]);
 
+  const updateField = useCallback(
+    (x: number, y: number) => {
+      dispatch({ type: 'FIELD_OPENED', payload: { x, y } });
+    },
+    [dispatch]
+  );
+
   return (
     <div className="game-container">
-      {grid.map(row => (
-        <div className="row">
-          {row.map(field => (
-            <div data-testId="field" className="field">
+      {grid.map((row, y) => (
+        <div key={y} className="row">
+          {row.map((field, x) => (
+            <div
+              key={field.id}
+              data-testid="field"
+              className={`field ${field.isActive ? 'active' : ''}`}
+              onClick={() => updateField(x, y)}
+            >
               <span>{field.isBomb && 1}</span>
             </div>
           ))}
